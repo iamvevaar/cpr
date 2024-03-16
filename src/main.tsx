1073e2f5a84af7b0d9c89eb7b5a2c76b9467e34e
iamvevaar <gautam15suthar@gmail.com>
2024-03-16 12:49:30 +0530

    analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics/>
     <SpeedInsights/>
     <App />
   </React.StrictMode>,
