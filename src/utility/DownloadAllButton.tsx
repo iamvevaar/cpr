372a27592adc4923ea9c4978cd01e67f94c0750a
iamvevaar <gautam15suthar@gmail.com>
2024-03-19 00:45:25 +0530

    download buton optimzed

--- src/utility/DownloadAllButton.tsx
+++ src/utility/DownloadAllButton.tsx
@@ -6,27 +6,25 @@ interface DownloadAllButtonProps {
 
 const DownloadAllButton: React.FC<DownloadAllButtonProps> = ({ croppedImages }) => {
   const handleDownload = (croppedImage: string, index: number) => {
-    // Create a temporary anchor element
     const link = document.createElement("a");
     link.href = croppedImage;
-    link.download = `vevaar_${index}.jpg`;
-    
-    // Trigger a click event to start the download
-    document.body.appendChild(link);
+    link.download = `vevaar_${index+1}.jpg`;
     link.click();
-    document.body.removeChild(link);
   };
 
   return (
-        <div className="grid grid-cols-1 items-center">
-          {croppedImages.map((croppedImage, index) => (
-                <button key={index} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2 w-32 h-[64px]" onClick={() => handleDownload(croppedImage, index)}>
-                  Download Image {index + 1}
-                </button>
-              ))}
-            </div>
-          );
-        };
-        
-        export default DownloadAllButton;
-        
+    <div className="grid grid-cols-1 items-center">
+      {croppedImages.map((croppedImage, index) => (
+        <button
+          key={index}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2 w-32 h-[64px]"
+          onClick={() => handleDownload(croppedImage, index)}
+        >
+          Download Image {index + 1}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default DownloadAllButton;
